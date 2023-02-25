a5a3474205428925fe29d52e5eb3e454f6e5f3c4
Eldos <eldos.baktybekov@gmail.com>
2023-02-25 11:38:23 +0600

    :sparkles: Add telegram api service (#GI-25)

--- src/telegram.py
+++ src/telegram.py
@@ -2,7 +2,12 @@ import contextlib
 
 import httpx
 
-__all__ = ('closing_telegram_api_client',)
+import models
+
+__all__ = (
+    'closing_telegram_api_client',
+    'TelegramAPIService',
+)
 
 
 @contextlib.contextmanager
@@ -13,3 +18,16 @@ def closing_telegram_api_client(*, bot_token: str) -> httpx.Client:
     base_url = f'https://api.telegram.org/bot{bot_token}/'
     with httpx.Client(base_url=base_url) as client:
         yield client
+
+
+class TelegramAPIService:
+
+    def __init__(self, api_client: httpx.Client):
+        self.__api_client = api_client
+
+    def get_chat(self, *, chat_id: int) -> models.Chat:
+        url = '/getChat'
+        request_query_params = {'chat_id': chat_id}
+        response = self.__api_client.get(url, params=request_query_params)
+        response_data = response.json()
+        return models.Chat.from_response_data(response_data['result'])
