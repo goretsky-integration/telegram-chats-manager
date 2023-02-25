c2df934a68c7c24b7e0a51703567975f2f79e798
Eldos <eldos.baktybekov@gmail.com>
2023-02-25 13:55:45 +0600

    :sparkles: Add property to get chat full name (#GI-25)

--- src/models.py
+++ src/models.py
@@ -13,6 +13,20 @@ class Chat:
     first_name: str | None
     last_name: str | None
 
+    @property
+    def full_name(self) -> str:
+        """Get full name of the Chat.
+        For private chat it is first_name + last_name.
+        For other chat types it is title.
+        """
+        if self.title is not None:
+            return self.title
+
+        if self.last_name is not None:
+            return f'{self.first_name} {self.last_name}'
+
+        return f'{self.first_name}'
+
     @classmethod
     def from_response_data(cls, response_data: dict) -> Self:
         return cls(
