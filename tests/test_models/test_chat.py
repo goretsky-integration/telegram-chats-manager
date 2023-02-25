180e1c934765a09202a66ac04ad1d073cc1e43a1
Eldos <eldos.baktybekov@gmail.com>
2023-02-25 14:03:24 +0600

    :white_check_mark: Add tests for `full_name` property (#GI-25)

--- tests/test_models/test_chat.py
+++ tests/test_models/test_chat.py
@@ -45,3 +45,37 @@ import models
 )
 def test_chat_from_response_data_constructor(response_data, model):
     assert models.Chat.from_response_data(response_data) == model
+
+
+@pytest.mark.parametrize(
+    'chat, full_name',
+    [
+        (
+                models.Chat(
+                    id=1234,
+                    type='private',
+                    first_name='Alex',
+                ),
+                'Alex',
+        ),
+        (
+                models.Chat(
+                    id=2345,
+                    type='supergroup',
+                    title='Friends',
+                ),
+                'Friends',
+        ),
+        (
+                models.Chat(
+                    id=3456,
+                    type='private',
+                    first_name='Benedict',
+                    last_name='Cumberbatch',
+                ),
+                'Benedict Cumberbatch',
+        ),
+    ]
+)
+def test_chat_full_name_property(chat: models.Chat, full_name: str):
+    assert chat.full_name == full_name
