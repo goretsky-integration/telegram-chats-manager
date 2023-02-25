e5496c3e4d09695d5a7feff06e2e9bda09589330
Eldos <eldos.baktybekov@gmail.com>
2023-02-25 16:01:16 +0600

    :white_check_mark: Add tests (#GI-25)

--- tests/test_database_api/test_database_api_service.py
+++ tests/test_database_api/test_database_api_service.py
@@ -11,6 +11,12 @@ def assert_all_responses_were_requested() -> bool:
     return False
 
 
+@pytest.fixture
+def database_api_service():
+    with httpx.Client(base_url='http://localhost:8000') as client:
+        yield DatabaseAPIService(api_client=client)
+
+
 @pytest.mark.parametrize(
     'response_data',
     [
@@ -20,16 +26,13 @@ def assert_all_responses_were_requested() -> bool:
         },
     ]
 )
-def test_database_api_service_get_telegram_chats_last_page(httpx_mock, response_data):
+def test_database_api_service_get_telegram_chats_last_page(httpx_mock, response_data, database_api_service):
     httpx_mock.add_response(json=response_data, method='GET')
     expected = [models.TelegramChat.from_response_data(chat) for chat in response_data['telegram_chats']]
-
-    with httpx.Client(base_url='http://localhost:8000') as client:
-        database_api_service = DatabaseAPIService(api_client=client)
-        assert database_api_service.get_telegram_chats() == expected
+    assert database_api_service.get_telegram_chats() == expected
 
 
-def test_database_api_service_get_telegram_chats_pagination(httpx_mock):
+def test_database_api_service_get_telegram_chats_pagination(httpx_mock, database_api_service):
     is_last_page = False
 
     def get_response(request: httpx.Request):
@@ -48,18 +51,33 @@ def test_database_api_service_get_telegram_chats_pagination(httpx_mock):
     httpx_mock.add_callback(get_response)
     expected = [models.TelegramChat.from_response_data(chat) for chat in
                 [{'chat_id': 2345, 'title': 'Steve'}, {'chat_id': 1234, 'title': 'Alex'}]]
-
-    with httpx.Client(base_url='http://localhost:8000') as client:
-        database_api_service = DatabaseAPIService(api_client=client)
-        assert database_api_service.get_telegram_chats() == expected
+    assert database_api_service.get_telegram_chats() == expected
 
 
-def test_database_api_service_raises_error(httpx_mock):
+def test_database_api_service_raises_error(httpx_mock, database_api_service):
     httpx_mock.add_response(status_code=400)
 
-    with httpx.Client(base_url='http://localhost:8000') as client:
-        database_api_service = DatabaseAPIService(api_client=client)
+    with pytest.raises(exceptions.DatabaseAPIError) as error:
+        database_api_service.get_telegram_chats()
+    assert error.value.args[0] == 'Could not get telegram chats page'
 
-        with pytest.raises(exceptions.DatabaseAPIError) as error:
-            database_api_service.get_telegram_chats()
-        assert error.value.args[0] == 'Could not get telegram chats page'
+
+def test_database_api_service_update_chat(httpx_mock, database_api_service):
+    httpx_mock.add_response(status_code=204)
+    try:
+        database_api_service.update_telegram_chat(chat_id=1234, title='Alex')
+    except exceptions.DatabaseAPIError:
+        pytest.fail('Error raised')
+
+
+@pytest.mark.parametrize(
+    'status_code',
+    [
+        400, 401, 404, 500, 502
+    ]
+)
+def test_database_api_service_update_chat_error_raised(httpx_mock, database_api_service, status_code):
+    httpx_mock.add_response(status_code=status_code)
+    with pytest.raises(exceptions.DatabaseAPIError) as error:
+        database_api_service.update_telegram_chat(chat_id=1234, title='Alex')
+    assert error.value.args[0] == 'Could not update telegram chat'
